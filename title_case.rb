ca0b11c085c2f09db193a8c0ae8dd2c7b894c804
Frances Maxwell <fmaxwell@gmail.com>
2016-09-24 03:12:04 +0100

    Committing title_case

--- title_case.rb
+++ title_case.rb
@@ -1,10 +1,24 @@
-def title_case(title, minor_words)
-
+def title_case(title, minor_words = '')
+  if !title.empty?
+    arr = title.split(" ")
+    new_arr = []
+    count = 1
+    arr.each do |word|
+      if count > 1 && (minor_words.downcase.match(/\b#{word.downcase}\b/))
+        new_arr << word.downcase
+      else
+        new_arr << word.capitalize
+      end
+      count += 1
+    end
+    new_arr.join(" ")
+  else
+    ""
+  end
 end
-
-
 
 puts title_case('')
 puts title_case('a clash of KINGS', 'a an the of')
 puts title_case('THE WIND IN THE WILLOWS', 'The In')
 puts title_case('the quick brown fox')
+puts title_case('First a of in', 'an often into')
